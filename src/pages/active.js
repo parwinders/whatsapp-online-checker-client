78e794bbcaadf76395810c8e2fabc84aa7f69ab4
Parwinder <psroadheaven@gmail.com>
2021-09-19 03:01:10 +0530

    added sound toggle

--- src/pages/active.js
+++ src/pages/active.js
@@ -2,12 +2,17 @@ import React from "react";
 import uuid from "uuid";
 import { List } from "framework7-react";
 import UserElement from "./userElement";
-const Active = ({ userList, remove }) => {
+const Active = ({ userList, remove, beep }) => {
     return (
         <List>
             {userList.length > 0 &&
                 userList.map((user) => (
-                    <UserElement key={uuid.v4()} user={user} remove={remove} />
+                    <UserElement
+                        key={uuid.v4()}
+                        beep={beep}
+                        user={user}
+                        remove={remove}
+                    />
                 ))}
         </List>
     );
